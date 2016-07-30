55e6dba639795cef596be963a0d85cc035fa8312
khanhtn102 <khanhtn102@gmail.com>
2016-07-30 20:54:36 +0700

    Add database vs Code sample

---
+++ VehicleRental/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VehicleRental.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int VehicleId { get; set; }
+        public string Name { get; set; }
+        public string IdentifyId { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string AddressDeliver { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
