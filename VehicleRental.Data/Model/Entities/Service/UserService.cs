aa8683c2c28327c6fb59e864c16e209351184ebc
khanhtn102 <khanhtn102@gmail.com>
2016-08-02 17:42:20 +0700

    Create Data Access layer

---
+++ VehicleRental.Data/Model/Entities/Service/UserService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleRental.Data.Model.Entities.Service
+{
+    public interface IUserService
+    {
+        List<User> GetList();
+        User CheckLogin(string username, string password);
+    }
+
+    public class UserService : IUserService
+    {
+
+        VehicleRentalEntities db = null;
+
+        public UserService()
+        {
+            db = new VehicleRentalEntities();
+        }
+
+        public User CheckLogin(string username, string password)
+        {
+            return db.Users.Where(n => n.Username == username && n.Password == password).SingleOrDefault();
+        }
+
+        public List<User> GetList()
+        {
+            return db.Users.ToList();
+        }
+    }
+}
---
+++ VehicleRental/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VehicleRental.Data.Model.Entities.Service;
+
+namespace VehicleRental.Controllers
+{
+    public class UserController : Controller
+    {
+
+        private IUserService Service = null;
+        
+        public UserController()
+        {
+            this.Service = new UserService();
+        }
+
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetListUser()
+        {
+            return Json(Service.GetList(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
